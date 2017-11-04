cee0b20d32b70b5688123a60ae1c21d4b4ae31c5
Seanie96 <semcdona@tcd.ie>
2017-11-04 15:07:28 +0000

    Exercise 1 done of first lab

--- Prolog/exercise1.pl
+++ Prolog/exercise1.pl
@@ -2,12 +2,11 @@ numeral(0).
 numeral(s(X)) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
 
-addTwo(0, 0, 0).
-addTwo(s(X), Y, s(Z)) :- addTwo(X,Y,Z), numeral(X), numeral(Y).
-addTwo(X, s(Y), s(Z)) :- addTwo(X,Y,Z), numeral(Y), numeral(X).
-addTwo(s(0)+X, Y, s(Z)) :- addTwo(X,Y,Z), numeral(X), numeral(Y).
-addTwo(X, s(0)+Y, s(Z)) :- addTwo(X,Y,Z), numeral(Y), numeral(X).
-addTwo(X, s(0+Y), s(Z)) :- addTwo(X,Y,Z), numeral(Y), numeral(X).
-
 add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
+
+add2(0,0,0).
+add2(0,s(Y),s(Z)) :- add2(0,Y,Z).
+add2(0, X+T, A) :- add(X,T,A),add2(0,A,A).
+add2(s(X),Y,s(R)) :- add2(X,Y,R).
+add2(X+Y,Z,R) :- add(X,Y,A),add2(A,Z,R).
